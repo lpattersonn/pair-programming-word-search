19a4ff718ab6673ebdb7934e990b6771980aa20e
Leshan Patterson <leshan0627@gmail.com>
2021-12-06 15:17:10 +0000

    Completed word search appusing unit testing to validate code

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -22,16 +22,16 @@ describe("#wordSearch()", function() {
 
   it("should return true if the word is present", function() {
     const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['A', 'W', 'C', 'F', 'Q', 'S', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['Y', 'F', 'C', 'F', 'Q', 'I', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'N', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'F', 'R', 'L'],
       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+      ['U', 'B', 'T', 'W', 'A', 'L', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'D', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', '', 'A', 'L'],
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+let arr = [];
+let arrTwo = arr.map(arr => arr.join(''));
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  // Check horizonatal axis for matching words
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  if (horizontalJoin.includes(word)) {
+    return horizontalJoin.includes(word);
+  } if (!horizontalJoin.includes(word)) {
+    return horizontalJoin.includes(word);
+  }
+  // Convert letters on vertical axis in to a new string
+  for (let i = 0; i < letters.length; i++) {
+    for (let z = 0; z < letters[i].length; z++) {
+      if (!Array.isArray(arr[i])) {
+        arr[i] = [];
+        arr[i].push(letters[z][i]);
+      } else {
+        arr[i].push(letters[z][i]);
+      }
+    }
+  }
+  // Check vertical axis for matching words
+  if (arrTwo.includes(word)) {
+    return (arrTwo.includes(word));
+  } if (!arrTwo.includes(word)) {
+    return arrTwo.includes(word);
+  }
+}; console.log(wordSearch([['a', 'b', 'w'],
+  ['b', 'e', 'e'],
+  ['b', 'e', 'n']], 'bee'
+));
+
+module.exports = wordSearch;
